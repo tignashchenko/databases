467ccc840476c9b86107dc672cdd5e6477cf125a
Hack Reactor Student <student@hackreactor.com>
2017-06-12 21:05:39 -0700

    Adds visualization and sets up initial schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,30 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id int,
+  messages varchar(255),
+  user_id int,
+  room_id int,
+  PRIMARY KEY (message_id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  user_id int,
+  user varchar(255),
+  PRIMARY KEY (user_id)  
+);
 
+CREATE TABLE rooms (
+  room_id int,
+  room varchar(255),
+  PRIMARY KEY (room_id)
+);
 
 
 
