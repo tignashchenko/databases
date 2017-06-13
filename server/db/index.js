72e341b12ad775db13cc4ee3b68021170f087798
Hack Reactor Student <student@hackreactor.com>
2017-06-12 21:55:02 -0700

    Establish connection with database

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,21 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  // host: 'http://127.0.0.1/3000',
+  user: 'root',
+  password: 'plantlife',
+  database: 'chat'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to database.');
+});
+
+
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
--- server/schema.sql
+++ server/schema.sql
@@ -4,27 +4,103 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-  message_id int,
-  messages varchar(255),
-  user_id int,
-  room_id int,
-  PRIMARY KEY (message_id)
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--   message_id int,
+--   messages varchar(255),
+--   user_id int,
+--   room_id int,
+--   PRIMARY KEY (message_id),
+--   FOREIGN KEY (user_id) REFERENCES users (user_id),
+--   FOREIGN KEY (room_id) REFERENCES rooms (room_id)
+-- );
+
+-- /* Create other tables and define schemas for them here! */
+-- CREATE TABLE users (
+--   user_id int,
+--   user varchar(255),
+--   PRIMARY KEY (user_id)  
+-- );
+
+-- CREATE TABLE rooms (
+--   room_id int,
+--   room varchar(255),
+--   PRIMARY KEY (room_id)
+-- );
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `message_id` INTEGER,
+  `messages` VARCHAR(255),
+  `user_id` INTEGER,
+  `room_id` INTEGER,
+  PRIMARY KEY (`message_id`)
 );
 
-/* Create other tables and define schemas for them here! */
-CREATE TABLE users (
-  user_id int,
-  user varchar(255),
-  PRIMARY KEY (user_id)  
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` INTEGER,
+  `user` VARCHAR(255),
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE rooms (
-  room_id int,
-  room varchar(255),
-  PRIMARY KEY (room_id)
+-- ---
+-- Table 'room'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `room`;
+    
+CREATE TABLE `room` (
+  `room_id` INTEGER,
+  `room` VARCHAR(255),
+  PRIMARY KEY (`room_id`)
 );
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `room` (`room_id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `room` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`message_id`,`messages`,`user_id`,`room_id`) VALUES
+-- ('','','','');
+-- INSERT INTO `users` (`id`,`user`) VALUES
+-- ('','');
+-- INSERT INTO `room` (`room_id`,`room`) VALUES
+-- ('','');
 
 
 
